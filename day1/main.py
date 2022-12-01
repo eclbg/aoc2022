5e639d95fb0e3b5cd11c7cf1d3baea757bafcf15
Enric Calabuig <ecalabuig@bmat.com>
2022-12-01 21:25:26 +0100

    TIL .split() also removes newlines

--- day1/main.py
+++ day1/main.py
@@ -1,7 +1,7 @@
 def part1():
     with open("input.txt") as f:
         curr_elf_cals = most_cals = 0
-        for row in map(lambda x: x.strip('\n'), f):
+        for row in map(lambda x: x.strip(), f):
             if not row:
                 most_cals = max(most_cals, curr_elf_cals)
                 curr_elf_cals = 0
@@ -15,7 +15,7 @@ def part2():
     with open("input.txt") as f:
         top_3_elves_cals = [0, 0, 0]
         curr_elf_cals = 0
-        for row in map(lambda x: x.strip('\n'), f):
+        for row in map(lambda x: x.strip(), f):
             if not row:
                 top_3_elves_cals = sorted(
                     top_3_elves_cals + [curr_elf_cals],
