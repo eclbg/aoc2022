ddb1b1f4bedf2048707ad70b87962eee0569c277
Enric Calabuig <ecalabuig@bmat.com>
2022-12-01 12:59:15 +0100

    Day 1 good solutions

--- day1/main.py
+++ day1/main.py
@@ -1,26 +1,36 @@
-def aggregate_elves_calories(filename):
+def part1():
     with open("input.txt") as f:
-        elf_cals = []
-        curr_elf_cals = 0
-        for row in map(lambda x: x.strip('\n'), f.readlines()):
-            if row:
-                curr_elf_cals += int(row)
-            else:
-                elf_cals.append(curr_elf_cals)
+        curr_elf_cals = most_cals = 0
+        for row in map(lambda x: x.strip('\n'), f):
+            if not row:
+                most_cals = max(most_cals, curr_elf_cals)
                 curr_elf_cals = 0
-    return elf_cals
+            else:
+                curr_elf_cals += int(row)
+        most_cals = max(most_cals, curr_elf_cals)
+    return most_cals
 
 
-def part1(elves_calories):
-    return max(elves_calories)
-
-
-def part2(elves_calories):
-    return sum(sorted(elves_calories, reverse=True)[:3])
+def part2():
+    with open("input.txt") as f:
+        top_3_elves_cals = [0, 0, 0]
+        curr_elf_cals = 0
+        for row in map(lambda x: x.strip('\n'), f):
+            if not row:
+                top_3_elves_cals = sorted(
+                    top_3_elves_cals + [curr_elf_cals],
+                    reverse=True
+                )[:3]
+                curr_elf_cals = 0
+            else:
+                curr_elf_cals += int(row)
+        top_3_elves_cals = sorted(
+            top_3_elves_cals + [curr_elf_cals],
+            reverse=True
+        )[:3]
+        return sum(top_3_elves_cals)
 
 
 if __name__ == "__main__":
-    filename = "input.txt"
-    elves_calories = aggregate_elves_calories(filename)
-    print(part1(elves_calories))
-    print(part2(elves_calories))
+    print(part1())
+    print(part2())
