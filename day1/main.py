2d6f6c013d853d81cc037b0113dd1173ddc293fb
Enric Calabuig <ecalabuig@bmat.com>
2022-12-01 09:10:57 +0100

    Day 1 part 2

--- day1/main.py
+++ day1/main.py
@@ -10,5 +10,20 @@ def part1():
                 curr_elf_cals = 0
         return max(elf_cals)
 
+
+def part2():
+    with open('input.txt') as f:
+        elf_cals = []
+        curr_elf_cals = 0
+        for row in map(lambda x: x.strip('\n'), f.readlines()):
+            if row:
+                curr_elf_cals += int(row)
+            else:
+                elf_cals.append(curr_elf_cals)
+                curr_elf_cals = 0
+        return sum(sorted(elf_cals, reverse=True)[:3])
+
+
 if __name__ == "__main__":
     print(part1())
+    print(part2())
