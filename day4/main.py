928d71f888cfa6b78b6cf791e417573f0769762e
Enric Calabuig <ecalabuig@bmat.com>
2022-12-04 17:00:06 +0100

    Day 4

---
+++ day4/main.py
@@ -0,0 +1,41 @@
+def part1():
+    with open("input.txt") as f:
+        count = 0  # count pairs where one assignment fully contains the other
+        for row in f:
+            # import pdb; pdb.set_trace()  # fmt: skip
+            row = row.strip()
+            ((start1, end1), (start2, end2)) = tuple(
+                map(lambda x: int(x), x.split("-")) for x in row.split(",")
+            )
+            if start1 == start2:
+                count += 1
+            elif start1 > start2:
+                if end1 <= end2:
+                    count += 1
+            elif start1 < start2:
+                if end2 <= end1:
+                    count += 1
+        return count
+
+
+def part2():
+    with open("input.txt") as f:
+        count = 0  # count pairs where one assignment fully contains the other
+        for row in f:
+            # import pdb; pdb.set_trace()  # fmt: skip
+            row = row.strip()
+            ((start1, end1), (start2, end2)) = tuple(
+                map(lambda x: int(x), x.split("-")) for x in row.split(",")
+            )
+            if start1 == start2:
+                count += 1
+            elif start1 > start2 and end2 >= start1:
+                count += 1
+            elif start1 < start2 and end1 >= start2:
+                count += 1
+        return count
+
+
+if __name__ == "__main__":
+    print(part1())
+    print(part2())
