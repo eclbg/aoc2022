d1b83fc7f97d68dd52c45eada9dc65a65ee2ddf4
Enric Calabuig <ecalabuig@bmat.com>
2022-12-06 18:48:13 +0100

    Day 4: clean part 1, optimize part 2

--- day4/main.py
+++ day4/main.py
@@ -7,14 +7,13 @@ def part1():
             ((start1, end1), (start2, end2)) = tuple(
                 map(lambda x: int(x), x.split("-")) for x in row.split(",")
             )
+
             if start1 == start2:
                 count += 1
-            elif start1 > start2:
-                if end1 <= end2:
-                    count += 1
-            elif start1 < start2:
-                if end2 <= end1:
-                    count += 1
+            elif start1 > start2 and end1 <= end2:
+                count += 1
+            elif start1 < start2 and end2 <= end1:
+                count += 1
         return count
 
 
@@ -27,11 +26,7 @@ def part2():
             ((start1, end1), (start2, end2)) = tuple(
                 map(lambda x: int(x), x.split("-")) for x in row.split(",")
             )
-            if start1 == start2:
-                count += 1
-            elif start1 > start2 and end2 >= start1:
-                count += 1
-            elif start1 < start2 and end1 >= start2:
+            if start1 <= end2 and end1 >= start2:
                 count += 1
         return count
 
